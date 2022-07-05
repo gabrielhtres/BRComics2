ab354e511d4df65e6be66e1d5fb5ea04c8404c48
Gabriel <gabrielhtres@gmail.com>
2022-07-05 15:17:31 -0300

    Iniciada configuração inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
+import { ROUTES } from './app.routes'
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopoComponent } from './topo/topo.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopoComponent,
+    RodapeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from "@angular/router";
 
+import { HomeComponent } from "./home/home.component";
+
 export const ROUTES: Routes = [
-    {
-        path: 'home',
-        component: ''
-    }
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent }
 ]
