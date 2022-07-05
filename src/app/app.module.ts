5fcd792e3e9d5a8076f7a023418d930f67487123
Gabriel <gabrielhtres@gmail.com>
2022-07-05 15:35:11 -0300

    Instalado e testado Bootstrap e suas dependências

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TopoComponent } from './topo/topo.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { DashboardComponent } from './home/dashboard/dashboard.component';
+import { MenuComponent } from './home/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TopoComponent,
-    RodapeComponent
+    RodapeComponent,
+    DashboardComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
